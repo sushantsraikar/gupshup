b46c12835c5941b170cca96e862ac843335283cb
Sushant Raikar <sushant.raikar@housing.com>
2016-03-11 17:15:20 +0530

    set limit as 14

--- lib/gupshup.rb
+++ lib/gupshup.rb
@@ -61,6 +61,8 @@ module Gupshup
       number = opts[:send_to]
       msg_type = opts[:msg_type] || 'TEXT'
       
+      return false, 'Phone Number is too short' if number.to_s.length < 12
+      return false, 'Phone Number is too long' if number.to_s.length > 14
       return false, 'Phone Number should be numerical value' unless number.to_i.to_s == number.to_s
       return false, 'Message should be less than 725 characters long' if msg.to_s.length > 724
       call_api opts.merge({ :method => 'sendMessage' })
